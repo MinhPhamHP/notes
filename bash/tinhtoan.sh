b4d4292954217c1d4106e99a3d31e4042c87910e
Minh Pham <phamminh127@gmail.com>
2024-09-22 06:33:21 +0700

    update

---
+++ bash/tinhtoan.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+# nhap du lieu
+read -p "Nhap vao so thu nhat: " a 
+read -p "Nhap vao so thu hai: " b
+
+# Tinh toan
+tong=$(($a + $b))
+hieu=$(($a - $b))
+echo "$a + $b = $tong"
+echo "$a - $b = $hieu"
